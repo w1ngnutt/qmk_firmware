5ab784567a573338aeae52cb12bcd0881a0f7648
Justin Milam <jmilam@republicwireless.com>
2017-08-04 17:22:10 -0400

    tap-n-hold more better

--- keyboards/ergodox/keymaps/w1ngnutt/config.h
+++ keyboards/ergodox/keymaps/w1ngnutt/config.h
@@ -40,7 +40,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define DEBUG_MATRIX_SCAN_RATE
 #define ONESHOT_TAP_TOGGLE 2
 #define ONESHOT_TIMEOUT 2000
-#define IGNORE_MOD_TAP_INTERRUPT
-#define PERMISSIVE_HOLD
+
+// #define IGNORE_MOD_TAP_INTERRUPT
+// #define PERMISSIVE_HOLD
 
 #endif
