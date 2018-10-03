2d61758d449c87b749944bf6fa253bcda0a2db44
Justin Milam <jmilam@republicwireless.com>
2018-10-03 14:28:58 -0400

    update to latest

--- keyboards/ergodox_ez/keymaps/w1ngnutt/keymap.c
+++ keyboards/ergodox_ez/keymaps/w1ngnutt/keymap.c
@@ -1,4 +1,4 @@
-#include "ergodox.h"
+#include QMK_KEYBOARD_H
 #include "debug.h"
 #include "action_layer.h"
 #include "action_util.h"
@@ -80,10 +80,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                  |      |      |  FN  |           | PgDn |      |      |
  *                                  `--------------------'           `--------------------'
 */
-[BASE]=KEYMAP(//left half
+[BASE]=LAYOUT_ergodox(//left half
               KC_ESC,         KC_1,       KC_2,     KC_3,           KC_4,       KC_5,        KC_6,
               KC_GRV,         KC_Q,       KC_W,     KC_E,           KC_R,       KC_T,        M_TLDR,
-              KC_TAB,         KC_A,       KC_S,     KC_D,           KC_F,       KC_G,
+              KC_TAB,         KC_A,       KC_S,     KC_D,           KC_F,       ALT_T(KC_G),
               KC_LSFT,        KC_Z,       KC_X,     KC_C,           SFT_T(KC_V),CTL_T(KC_B),ALL_T(KC_LBRC),
               KC_FN1,         M_RFI,      KC_LCTRL, KC_LALT,        KC_LGUI,
                                                                                 M_TPST,   KC_DEL,
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
               //right half
               TG(MOUSE),      KC_7,       KC_8,       KC_9,           KC_0,       KC_MINS,  KC_EQL,
               KC_CAPS,        KC_Y,       KC_U,       KC_I,           KC_O,       KC_P,     KC_BSLS,
-                              KC_H,       KC_J,       KC_K,           KC_L,       KC_SCLN,  KC_QUOT,
+                              ALT_T(KC_H),KC_J,       KC_K,           KC_L,       KC_SCLN,  KC_QUOT,
               ALL_T(KC_RBRC), CTL_T(KC_N),SFT_T(KC_M),KC_COMM,        KC_DOT,     KC_SLSH,  KC_RSFT,
                               KC_RGUI,    KC_RALT,    CTL_T(KC_LBRC), KC_RBRC,    KC_FN1,
               KC_HOME,        KC_END,
@@ -120,7 +120,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                  |      |      |      |           | XXXX | XXXX | XXXX |
  *                                  `--------------------'           `--------------------'
  */
-[MOUSE]=KEYMAP(//left half
+[MOUSE]=LAYOUT_ergodox(//left half
                 KC_NO,   KC_NO,   KC_NO,        KC_NO,        KC_NO,      KC_NO,   KC_NO,
                 KC_NO,   KC_NO,   KC_MS_BTN2,   KC_MS_BTN2,   KC_MS_BTN1, KC_NO,   KC_NO,
                 KC_NO,   KC_NO,   KC_NO,        KC_NO,        KC_NO,      KC_NO,
@@ -162,7 +162,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *
  * XXX = These keys are transparent keys that, when pressed, they issue the key from the previous layer.
  */
-[FN]=KEYMAP(//left half
+[FN]=LAYOUT_ergodox(//left half
             KC_NO,   KC_F1,          KC_F2,               KC_F3,               KC_F4,                 KC_F5,  KC_F6,
             KC_NO,   KC_MEDIA_STOP,  KC_MEDIA_REWIND,     KC_MEDIA_SELECT,     KC_MEDIA_FAST_FORWARD, KC_NO,  KC_TRNS,
             KC_CAPS, KC_MEDIA_EJECT, KC_MEDIA_PREV_TRACK, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK,   KC_NO,
